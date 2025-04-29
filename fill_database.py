4a20c5aa99c22a1d466c0d81d332314d657ca41e
gabcro <gabcroliveira@gmail.com>
2025-04-29 14:00:11 -0300

    Restruturação fill_database.py

--- fill_database.py
+++ fill_database.py
@@ -9,7 +9,7 @@ area_map = {}
 curso_map = {}
 instituicao_map = {}
 #municipio_map = {}
-#uf_map = {}
+uf_map = {}
 #setor_map = {}
 
 def safe_int(value, default=None):
@@ -36,60 +36,48 @@ def safe_float(value, default=None):
     except (ValueError, TypeError):
         return default
 
-def load_education_data(cursor, connection, file_path):
+def load_education_data(cursor, connection, education_path):
     """ Load education data from a CSV file into the database.
 
     Args:
         cursor: The database cursor.
         connection: The database connection.
-        file_path: Path to the CSV file.
+        education_path: Path to the CSV file.
     """
-    print(f"\nLoading data from {file_path}...")
+    print(f"\nLoading data from {education_path}...")
     processed_rows = 0
     skipped_rows = 0
 
     try:
-        with open(file_path, 'r', encoding='utf-8') as csvfile:
+        with open(education_path, 'r', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile, delimiter=';')
 
             for row in reader:
                 try:
                     # Extract and clean data from the row
-                    area_cod = row['area_cod']
-                    nome_area_atuacao = row['nome_area_atuacao'].strip()
-
-                    curso_cod = row['curso_cod']
-                    curso_nome = row['curso_nome'].strip()
-                    grau_academico = row['grau_academico']
-                    modo_ensino = row['modo_ensino'].strip()
-
-                    inst_cod = row['inst_cod']
+                    inst_cod = safe_int(row['inst_cod'])
                     inst_nome = row['inst_nome'].strip()
+
                     categoria_adm = row['categoria_adm'].strip()
                     org_academica = row['org_academica'].strip()
 
-                    #municipio_cod = safe_int(row['municipio_cod'])
-                    #municipio_nome = row['municipio_nome'].strip()
-                    #uf_sigla = row['uf_sigla'].strip()
-                    #uf_nome = row['uf_nome'].strip()
+                    curso_cod = safe_int(row['curso_cod'])
+                    curso_nome = row['curso_nome'].strip()
+                    grau_academico = safe_int(row['grau_academico'])
+                    modo_ensino = row['modo_ensino'].strip()
 
-                    #setor_nome = row['setor_nome'].strip()
+                    area_cod = safe_int(row['area_cod'])
+                    nome_area_atuacao = row['nome_area_atuacao'].strip()
 
-                    #ano = safe_int(row['ano'])
-                    #num_pessoas_empregadas = safe_int(row['num_pessoas_empregadas'])
-                    #estab_total = safe_int(row['estab_total'])
-                    #estab_com_vinculo = safe_int(row['estab_com_vinculo'])
-                    #estab_sem_vinculo = safe_int(row['estab_sem_vinculo'])
-                    #media_remuneracao = safe_float(row['media_remuneracao'])
-
+                    ano_referencia = safe_int(row['ano_referencia'])
                     num_ingressantes = safe_int(row['num_ingressantes'])
                     num_concluintes = safe_int(row['num_concluintes'])
                     taxa_desistencia = safe_float(row['taxa_desistencia'])
 
-                    #grau_relacao = safe_float(row['grau_relacao'])
+                    uf_nome = row['uf_nome'].strip()
+                    uf_sigla = row['uf_sigla'].strip()
 
-                    '''
-                    # Insere Unidade Federativa
+                    # Inserts Unidade Federativa
                     if uf_sigla and uf_sigla not in uf_map:
                         cursor.execute(
                             """
@@ -100,7 +88,8 @@ def load_education_data(cursor, connection, file_path):
                             (uf_sigla, uf_nome)
                         )
                         uf_map[uf_sigla] = uf_nome
-
+                    
+                    '''
                     # Insere Municipio
                     if municipio_cod and municipio_cod not in municipio_map:
                         cursor.execute(
@@ -112,10 +101,9 @@ def load_education_data(cursor, connection, file_path):
                             (municipio_cod, municipio_nome, uf_sigla)
                         )
                         municipio_map[municipio_cod] = municipio_nome
-
                     '''
 
-                    # Insere Area_Atuacao
+                    # Inserts Area_Atuacao
                     if area_cod and area_cod not in area_map:
                         cursor.execute(
                             """
@@ -127,19 +115,19 @@ def load_education_data(cursor, connection, file_path):
                         )
                         area_map[area_cod] = nome_area_atuacao
 
-                    # Insere Instituicao_Superior
+                    # Insert Instituicao_Superior
                     if inst_cod and inst_cod not in instituicao_map:
                         cursor.execute(
                             """
-                            INSERT INTO public."Instituicao_Superior" (inst_cod, inst_nome, categoria_adm, org_academica)
-                            VALUES (%s, %s, %s, %s)
+                            INSERT INTO public."Instituicao_Superior" (inst_cod, inst_nome, categoria_adm, org_academica, uf_sigla)
+                            VALUES (%s, %s, %s, %s, %s)
                             ON CONFLICT (inst_cod) DO NOTHING;
                             """,
-                            (inst_cod, inst_nome, categoria_adm, org_academica)
+                            (inst_cod, inst_nome, categoria_adm, org_academica, uf_sigla)
                         )
                         instituicao_map[inst_cod] = inst_nome
 
-                    # Insere Curso
+                    # Inserts Curso
                     if curso_cod and curso_cod not in curso_map:
                         cursor.execute(
                             """
@@ -178,17 +166,6 @@ def load_education_data(cursor, connection, file_path):
                             (ano, municipio_cod, setor_nome, num_pessoas_empregadas)
                         )
 
-                    # Insere Estabelecimento_Economico_Por_UF
-                    if ano and uf_sigla:
-                        cursor.execute(
-                            """
-                            INSERT INTO public."Estabelecimento_Economico_Por_UF" (ano, uf_sigla, estab_total, estab_com_vinculo, estab_sem_vinculo)
-                            VALUES (%s, %s, %s, %s, %s)
-                            ON CONFLICT (ano) DO NOTHING;
-                            """,
-                            (ano, uf_sigla, estab_total, estab_com_vinculo, estab_sem_vinculo)
-                        )
-
                     # Insere Remuneracao_Media_Por_UF
                     if ano and uf_sigla and media_remuneracao is not None:
                         cursor.execute(
@@ -201,8 +178,8 @@ def load_education_data(cursor, connection, file_path):
                         )
                     '''
 
-                    '''
-                    # Insere Trajetoria_Curso
+                
+                    # Inserts Trajetoria_Curso
                     if curso_cod and ano:
                         cursor.execute(
                             """
@@ -210,9 +187,8 @@ def load_education_data(cursor, connection, file_path):
                             VALUES (%s, %s, %s, %s, %s)
                             ON CONFLICT (curso_cod, ano_referencia) DO NOTHING;
                             """,
-                            (curso_cod, ano, num_ingressantes, num_concluintes, taxa_desistencia)
+                            (curso_cod, ano_referencia, num_ingressantes, num_concluintes, taxa_desistencia)
                         )
-                    '''
 
                     '''
                     # Insere Relacao_Area_Setor
@@ -242,13 +218,13 @@ def load_education_data(cursor, connection, file_path):
                     sys.stdout.flush()
 
     except FileNotFoundError:
-        print(f"Error: File not found at {file_path}")
+        print(f"Error: File not found at {education_path}")
         raise
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
         raise
     finally:
-        print(f"\nFinished loading {file_path}. Total rows processed: {processed_rows + skipped_rows}, Inserted: {processed_rows}, Skipped: {skipped_rows}.")
+        print(f"\nFinished loading {education_path}. Total rows processed: {processed_rows + skipped_rows}, Inserted: {processed_rows}, Skipped: {skipped_rows}.")
 
 def main(datasets_directory):
     """ Main function to load data into the database.
@@ -257,17 +233,18 @@ def main(datasets_directory):
         datasets_directory: Directory containing the datasets to load.
     """
     try:
-        connection = psycopg2.connect(
-            dbname='projeto_1',
-            user='postgres',
-            password='Maria1221@@',
-            host='localhost',
-            port='5432'
-        )
-        cursor = connection.cursor()
-
+        #connection = psycopg2.connect(
+        #    dbname='projeto_1',
+        #    user='postgres',
+        #    password='Maria1221@@',
+        #    host='localhost',
+        #    port='5432'
+        #)
+        #cursor = connection.cursor()
+        cursor = None
+        connection = None
         education_path = f'{datasets_directory}/indicadores_educacao.csv'
-        load_education_data(cursor, connection, file_path)
+        load_education_data(cursor, connection, education_path)
 
         rais_4_path = f'{datasets_directory}/rais_tabela4_joined.csv'
         rais_6_path = f'{datasets_directory}/rais_tabela6_joined.csv'
